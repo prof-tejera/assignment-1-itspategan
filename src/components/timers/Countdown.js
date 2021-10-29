959ea9c041c86c15c3e641f7311ed97769955399
itspategan <86270686+itspategan@users.noreply.github.com>
2021-10-28 20:33:08 -0400

    timers implemented
    timer display, button components, text and input components. Styling needs some work

--- src/components/timers/Countdown.js
+++ src/components/timers/Countdown.js
@@ -1,9 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import DisplayTime from "../generic/DisplayTime";
+import ControlButton from "../generic/ControlButton";
+import Input from "../generic/Input";
+import SimpleText from "../generic/SimpleText";
 
-class Countdown extends React.Component {
+const Container = styled.div`
+  height: 90%;
+  width: 100%;
+  border: none;
+  background: #8d99ae;
+  padding: 20px;
+  text-align: center;
+`;
+
+class Stopwatch extends React.Component {
   render() {
-    return null;
+    return (
+      <Container>
+        <DisplayTime></DisplayTime>
+        <div>
+          <ControlButton value={"start"} />
+          <ControlButton value={"pause"} />
+        </div>
+        <SimpleText value={"SET TIME"} />
+        <Input />
+      </Container>
+    );
   }
 }
 
-export default Countdown;
+export default Stopwatch;
--- src/components/timers/XY.js
+++ src/components/timers/XY.js
@@ -1,9 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import DisplayTime from "../generic/DisplayTime";
+import ControlButton from "../generic/ControlButton";
+import Input from "../generic/Input";
+import SimpleText from "../generic/SimpleText";
 
-class XY extends React.Component {
+const Container = styled.div`
+  height: 90%;
+  width: 100%;
+  border: none;
+  background: #8d99ae;
+  padding: 20px;
+  text-align: center;
+`;
+
+class Stopwatch extends React.Component {
   render() {
-    return null;
+    return (
+      <Container>
+        <DisplayTime></DisplayTime>
+        <div>
+          <ControlButton value={"start"} />
+          <ControlButton value={"pause"} />
+        </div>
+        <div className="block">
+          <SimpleText value={"ROUNDS"} />
+          <Input />
+          <SimpleText value={"TIME"} />
+          <Input />
+        </div>
+      </Container>
+    );
   }
 }
 
-export default XY;
+export default Stopwatch;
